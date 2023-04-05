37265fbe1d7f418e201bfd8b129d2daef8555a5a
Charles Cao <nebulablock@gmail.com>
2023-04-05 12:56:50 -0400

    add receive_job in LAD

--- computing_provider/api/lagrange_view.py
+++ computing_provider/api/lagrange_view.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from computing_provider.computing_worker.tasks.download_pokemon_sprite import (
     download_pokemon_sprite_task,
 )
@@ -27,6 +27,22 @@ def download_pokemon_sprite(pokemon_name):
     return jsonify({"taskId": task.id}), 202
 
 
+@pokemon_blueprint.post('/lagrange/jobs')
+def receive_job():
+    job_data = request.json
+    logging.info("Job received %s" % job_data)
+    # Process the job
+    result = process_job(job_data)
+
+    return jsonify(result), 200
+
+
+def process_job(job_data):
+    # Here, you can implement your custom logic to process the job_data
+    # For demonstration purposes, we'll simply return the job_data as the result
+    return job_data
+
+
 @pokemon_blueprint.get("/lagrange/space/<task_name>")
 def build_space(task_name):
     """
@@ -38,7 +54,7 @@ def build_space(task_name):
     task = build_space_task.delay(task_name)
     logger.info(f"build spaces task task created! Task ID: {task!r}")
 
-    return jsonify({"taskId": task.id, "endPoint": "https://" + task_name + ".crosschain.computer" }), 202
+    return jsonify({"taskId": task.id, "endPoint": "https://" + task_name + ".crosschain.computer"}), 202
 
 
 @pokemon_blueprint.delete("/lagrange/space/<task_name>")
@@ -46,6 +62,3 @@ def delete_task(task_name):
     delete_space_task(task_name)
 
     return jsonify({"space_name": task_name}), 202
-
-
-
